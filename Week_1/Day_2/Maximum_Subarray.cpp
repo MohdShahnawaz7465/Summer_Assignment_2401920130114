fb94753cc793bb4666dc5bdc00b11426dc6223be
MohdShahnawaz7465 <shane74570@gmail.com>
2026-06-19 12:00:05 +0530

    Create  Maximum_Subarray.cpp

---
+++ Week_1/Day_2/Maximum_Subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        long long sum=0;
+        long long maxi = LONG_MIN;
+        for(int i=0;i<nums.size();i++){
+            sum +=nums[i];
+            maxi = max(maxi, sum);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return (int) maxi;
+    }
+};

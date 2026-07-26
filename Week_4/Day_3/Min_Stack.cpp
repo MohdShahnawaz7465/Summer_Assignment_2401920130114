2b1bea57377adf192fd64595d50f32a7ad077269
MohdShahnawaz7465 <shane74570@gmail.com>
2026-07-26 18:49:36 +0530

    Create Min_Stack.cpp

---
+++ Week_4/Day_3/Min_Stack.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+public:
+    MinStack() {
+
+    }
+
+    void push(int value) {
+        st.push(value);
+        if (minSt.empty() || value <= minSt.top()) {
+            minSt.push(value);
+        } else {
+            minSt.push(minSt.top());
+        }
+    }
+
+    void pop() {
+        st.pop();
+        minSt.pop();
+    }
+
+    int top() {
+        return st.top();
+    }
+
+    int getMin() {
+        return minSt.top();
+    }
+
+private:
+    stack<int> st;
+    stack<int> minSt;
+};

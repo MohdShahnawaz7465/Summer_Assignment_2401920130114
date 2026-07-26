9c7978569fa1b2953e814a72d4afb12ca6b9e749
MohdShahnawaz7465 <shane74570@gmail.com>
2026-07-26 19:18:55 +0530

    Create Diameter_of_Binary_Tree.cpp

---
+++ Week_3/Day_4/Diameter_of_Binary_Tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        height(root, diameter);
+        return diameter;
+    }
+
+private:
+    int height(TreeNode* node, int& diameter) {
+        if (node == nullptr) {
+            return 0;
+        }
+
+        int leftHeight = height(node->left, diameter);
+        int rightHeight = height(node->right, diameter);
+
+        diameter = max(diameter, leftHeight + rightHeight);
+
+        return max(leftHeight, rightHeight) + 1;
+    }
+};
